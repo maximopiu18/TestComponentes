3e501d1e857e8b4d474c26f29fa95c317a3c5998
Alberto Martinez <97967830+AlbertoMartinezGlo@users.noreply.github.com>
2022-10-04 15:07:14 -0500

    Commit inicial
    Se sube proyecto principal

---
+++ app/src/main/java/com/empresa/componentes/Activity1.java
@@ -0,0 +1,25 @@
+package com.empresa.componentes;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity1 extends AppCompatActivity {
+    Button btn1;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla1);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("mensaje", " pulsaste el 1 desde java");
+            }
+        });
+    }
+}
